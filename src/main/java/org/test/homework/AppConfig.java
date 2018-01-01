8c24b0ae3cc9859097ca98cba05e5383976dd335
stanislavspipirs <stanislavs.pipirs@gmail.com>
2018-01-01 09:23:16 +0200

    Initial import

--- src/main/java/org/test/homework/AppConfig.java
+++ src/main/java/org/test/homework/AppConfig.java
@@ -4,18 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.AsyncRestTemplate;
 
-import java.time.Clock;
-
 @Configuration
 public class AppConfig {
 
     @Bean
     public AsyncRestTemplate restTemplate() {
         return new AsyncRestTemplate();
-    }
-
-    @Bean
-    public Clock clock() {
-        return Clock.systemUTC();
     }
 }
